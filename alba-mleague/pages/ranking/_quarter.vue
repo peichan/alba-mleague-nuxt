257e3d38745c451259bd44326357c27d07aeee53
peichan <ktgchyshnr13@gmail.com>
2020-05-30 19:22:02 +0900

    add 5/30

--- alba-mleague/pages/ranking/_quarter.vue
+++ alba-mleague/pages/ranking/_quarter.vue
@@ -81,6 +81,12 @@ class DayResult {
 
 const quarterResults: { [key: string]: DayResult[] } = {
   "2020q2": [
+    new DayResult("5/30 (土)", [
+      new Result("北口 善紀", 69),
+      new Result("金子 侑起", 18),
+      new Result("網 亮太朗", 1.5),
+      new Result("稲田 翼", -48.5)
+    ]),
     new DayResult("5/29 (金)", [
       new Result("清水 明彦", 114.5),
       new Result("祖父江 尚也", 99.5),
